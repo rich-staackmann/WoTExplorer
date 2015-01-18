ade44b9684e2f26d857a836a53d573b00846161f
rich-staackmann <rich.staackmann@gmail.com>
2015-01-18 16:40:17 -0600

    playing with nghint and watch counts

--- js/controllers/TankListCtrl.js
+++ js/controllers/TankListCtrl.js
@@ -19,4 +19,25 @@ controller('TankListCtrl', ['TankListService', '$scope', '$location', '$cacheFac
   $scope.openTankDetail = function (id) {
     $location.path('/tank/' + id);
   };
+
+  var getWatchers = (function () {
+      var root = angular.element('html');
+      var watchers = [];
+
+      var f = function (element) {
+        if (element.data().hasOwnProperty('$scope')) {
+          angular.forEach(element.data().$scope.$$watchers, function (watcher) {
+            watchers.push(watcher);
+          });
+        }
+
+        angular.forEach(element.children(), function (childElement) {
+          f($(childElement));
+        });
+      };
+
+      f(root);
+
+      console.log(watchers.length);
+    })();
 }]);
