0821b33012f9f6fe2871ca3f02f5e83ec69bb657
rich-staackmann <rich.staackmann@gmail.com>
2015-01-18 20:09:11 -0600

    Added CacheFactory

--- js/controllers/TankListCtrl.js
+++ js/controllers/TankListCtrl.js
@@ -1,12 +1,20 @@
 angular.module('wotExplorer').
-controller('TankListCtrl', ['TankListService', '$scope', '$location', function TankListCtrl (tankListService, $scope, $location) {
+controller('TankListCtrl', ['TankListService', '$scope', '$location', '$cacheFactory', function TankListCtrl (tankListService, $scope, $location, $cacheFactory) {
   $scope.tanks = {};
   $scope.numTanks = 0;
 
-  tankListService.getList().success(function (list) {
-    $scope.tanks = list.data;
-    $scope.numTanks = list.count;
-  });
+  if ($cacheFactory.get('TankList')) {
+    $scope.tanks = $cacheFactory.get('TankList').get('tanks');
+    $scope.numTanks = $cacheFactory.get('TankList').get('numTanks');
+  } else {
+    var cache = $cacheFactory('TankList');
+    tankListService.getList().success(function (list) {
+      $scope.tanks = list.data;
+      $scope.numTanks = list.count;
+      cache.put("tanks", list.data);
+      cache.put("numTanks", list.count);
+    });
+  }
 
   $scope.openTankDetail = function (id) {
     $location.path('/tank/' + id);
